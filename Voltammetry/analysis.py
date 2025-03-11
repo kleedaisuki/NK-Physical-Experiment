5070cfad3137e91e6ac45f77865a149cdd35da16
kleedaisuki <kleedaisuki@outlook.com>
2025-03-12 01:08:02 +0800

    Correcting formula
    + Corrected formula used in computing error in function.

--- Voltammetry/analysis.py
+++ Voltammetry/analysis.py
@@ -18,22 +18,22 @@ def analysis_U_I_data(path: str) -> None:
     I_list: list[float] = [float(number) * 0.001 for number in csv["I"]]
 
     # Computation.
-    r: float = mean([U_list[i] / I_list[i] for i in range(0, len(U_list))])
-    delta_U: float = max(U_list) * 0.02 * 0.01
-    delta_I: float = max(I_list) * 1.2 * 0.01
-    p: float = sqrt((delta_U / mean(U_list)) ** 2 + (delta_I / mean(I_list)) ** 2)
+    r_list: list[float] = [U_list[i] / I_list[i] for i in range(0, len(U_list))]
+    r: float = mean(r_list)
+    std: float = sqrt(
+        sum([(r_list[i] - r) ** 2 for i in range(len(r_list))]) / (len(r_list) - 1)
+    )
 
     # Write results to file.
-    with open(f"result_{path.removesuffix(".csv")}.ret", "a+") as result:
+    with open(f"{path.removesuffix(".csv")}.ret", "a+") as result:
         if result.tell() != 0:
             result.write(f"\n[{datetime.now()} statistics output]\n")
         else:
             result.write(f"[{datetime.now()} statistics output]\n")
         result.write(f"> R(average): {round(r, 2):.2f}\n")
-        result.write(f"> delta_U: +- {round(delta_U, 4):.4f} +- {4 * 0.0001}\n")
-        result.write(f"> delta_I: +- {round(delta_I, 2):.2f} +- {3 * 0.01}\n")
-        result.write(f"> px: {round(p * 100, 2):.2f}%\n")
-        result.write(f"Result: {round(r, 2):.2f} +- {round(r * p, 2):.2f}\n")
+        result.write(f"> std: +- {round(std, 2):.2f} +- {3 * 0.01}\n")
+        result.write(f"> px: {round(std / r * 100, 2):.2f}%\n")
+        result.write(f"Result: {round(r, 2):.2f} +- {round(std, 2):.2f}\n")
 
 
 def draw_linear(path: str) -> None:
@@ -43,7 +43,7 @@ def draw_linear(path: str) -> None:
     U_array: ndarray[ndarray[float]] = array([[number] for number in U_list])
     I_array: ndarray[float] = array([float(number) for number in csv["I"]])
 
-    # Set linear regression.
+    # Set linear regression
     model: LinearRegression = LinearRegression()
     model.fit(U_array, I_array)
     for_prediction: list[list[float]] = []
@@ -55,7 +55,7 @@ def draw_linear(path: str) -> None:
 
     # Init.
     plt.figure(figsize=(8, 6))
-    plt.title("Rx")
+    plt.title(f"{path.removesuffix(".csv")}")
     plt.xlabel("U/V", loc="right")
     plt.ylabel("I/mA", loc="top")
     plt.grid(True)
@@ -78,12 +78,12 @@ def draw_linear(path: str) -> None:
     # Output.
     plt.legend(loc="lower right")
     plt.savefig(
-        f"figure_{path.removesuffix(".csv")}.png",
+        f"linear_{path.removesuffix(".csv")}.png",
         format="png",
         bbox_inches="tight",
         dpi=480,
     )
-    with open(f"result_{path.removesuffix(".csv")}.ret", "a+") as result:
+    with open(f"{path.removesuffix(".csv")}.ret", "a+") as result:
         if result.tell() != 0:
             result.write(f"\n[{datetime.now()} figure output]\n")
         else:
@@ -124,7 +124,7 @@ def draw(path: str) -> None:
 
     # Init.
     plt.figure(figsize=(8, 6))
-    plt.title("Dx")
+    plt.title(f"{path.removesuffix(".csv")}")
     plt.xlabel("U/V", loc="right")
     plt.ylabel("I/mA", loc="top")
     plt.grid(True)
@@ -147,19 +147,19 @@ def draw(path: str) -> None:
     # Output.
     plt.legend(loc="lower right")
     plt.savefig(
-        f"figure_{path.removesuffix(".csv")}.png",
+        f"svr_{path.removesuffix(".csv")}.png",
         format="png",
         bbox_inches="tight",
         dpi=480,
     )
-    with open(f"result_{path.removesuffix(".csv")}.ret", "a+") as result:
+    with open(f"{path.removesuffix(".csv")}.ret", "a+") as result:
         if result.tell() != 0:
             result.write(f"\n[{datetime.now()} figure output]\n")
         else:
             result.write(f"[{datetime.now()} figure output]\n")
         result.write(".SVR(RBF)\n")
         result.write(f"> r: {model.score(U_array, I_array):.2f}\n")
-        result.write(f"Parameters:\n{grid_search.best_params_}\n")
+        result.write(f"Parameters:\n    {grid_search.best_params_}\n")
 
 
 def automatic(function) -> any:
